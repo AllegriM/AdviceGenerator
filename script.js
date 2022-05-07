1ffa8699cc16f4378454518a3de93ecf8bf8b222
Marco Allegri <elmonky99@gmail.com>
2022-05-07 03:00:13 -0300

    second commit, app finished

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
 // ADVICE API GENERATOR //
 
+let advNum = document.querySelector('#adv_Number');
 let advContainer = document.querySelector('.advContainer');
-let advNumber = document.querySelector('#adv_Number');
-let btn = document.querySelector('.diceBtn');
+let btnDice = document.querySelector('#btnAdv');
 
-fetch('https://api.adviceslip.com/advice')
-    .then( (resp) => resp.json() )
-    .then((data)=>{
-        let advNum = data.slip.id;
-        let advText =  data.slip.advice;
-        let advEl = document.createElement('p');
-        advEl.innerHTML = advText;
-        advContainer.appendChild(advEl);
-        advNumber.innerHTML = advNum;
-        btn.addEventListener('click', createAdvice)
-})
-
-function createAdvice() {
-    
+const fetchAdvice = async() => {
+    const res = await fetch('https://api.adviceslip.com/advice')
+    const data = await res.json()
+    btnDice.addEventListener('click', ()=>{
+        advNum.innerHTML = `${data.slip.id}`;
+        advContainer.innerHTML = `"${data.slip.advice}"`;
+    })
 }
